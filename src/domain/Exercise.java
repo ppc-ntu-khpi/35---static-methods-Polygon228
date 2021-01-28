6e7dd66d078bce6dcc70d7bd34d49228b7ceeb61
Polygon228 <sholohroman155@gmail.com>
2021-01-28 16:24:29 +0200

    +

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,25 @@
 package domain;
 
+import java.util.Arrays;
+
+/**
+ * Клас який представляє завдання
+ * @author Roman
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
+    
+    /**
+     * Перевіряє чи є одне слово анаграмою іншого
+     * @param firstWord перше слово
+     * @param secondWord друге слово
+     * @return логічне значення
+     */
+    public static boolean Calculate(String firstWord, String secondWord) {
+        char[] word1 = firstWord.toUpperCase().replaceAll("[\\s]", "").toCharArray();
+        char[] word2 = secondWord.toUpperCase().replaceAll("[\\s]", "").toCharArray();
+        Arrays.sort(word1);
+        Arrays.sort(word2);
+        return Arrays.equals(word1, word2);
+}
+    
 }
